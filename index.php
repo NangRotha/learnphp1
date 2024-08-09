8e295537871a8228d9ee45fce082766ffef268e7
rothanang21 <rothanang21@gmail.com>
2024-08-09 09:10:43 +0000

    2commit

--- index.php
+++ index.php
@@ -1,4 +1,33 @@
 <?php
-
-$name = getenv('NAME', true) ?: 'World';
-echo sprintf('Hello %s!', $name);
+   $ballance = 0.1;
+   $trip = 50;
+   if($ballance > $trip)
+   {
+    echo 'tv der lg';
+   }
+   else
+   {
+    echo 'ot tv der lg';
+   }
+   $myscore = 50;
+   if($myscore>=100)
+   {
+    echo 'pass A';
+   } 
+   elseif($myscore>=80)
+   {
+    echo 'pass B';
+   }
+   elseif($myscore>=60)
+   {
+    echo 'pass C';
+   }
+   elseif($myscore>=40)
+   {
+    echo 'pass D';
+   }
+   else
+   {
+    echo 'fail';
+   }
+?>
